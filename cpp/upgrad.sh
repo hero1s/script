045a0f0e801332a909e4b3d20aa7d859b86eb8be
toney.yin <toney.yin@idreamsky.com>
2022-06-20 10:16:13 +0800

    Update upgrad.sh

--- cpp/upgrad.sh
+++ cpp/upgrad.sh
@@ -3,11 +3,11 @@ set -xe
 
 #Linux之CentOS 7通过yum安装gcc
 yum install centos-release-scl -y
-yum install devtoolset-9-toolchain -y
-scl enable devtoolset-9 bash
-echo "source /opt/rh/devtoolset-9/enable" >> ~/.bash_profile
+yum install devtoolset-10-toolchain -y
+scl enable devtoolset-10 bash
+echo "source /opt/rh/devtoolset-10/enable" >> ~/.bash_profile
 source ~/.bash_profile
-source /opt/rh/devtoolset-9/enable
+source /opt/rh/devtoolset-10/enable
 
 
 #静态编译
